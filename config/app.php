b87bfabb459fed9ef8e0ede53f98711a767023b9
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2022-09-30 11:57:43 +0800

    update

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'ISUddContactTracing'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://tracing.cdd.edu.ph'),
 
     'asset_url' => env('ASSET_URL', null),
 
